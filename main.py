03c44e1744ae3702d32dd595ccafeaade85562ba
ew <97876437+ew-it1@users.noreply.github.com>
2023-05-17 18:19:13 +0200

    update testig streamlit cloud and path

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import os
+import sys
+st.write(sys.path)
 
 st.header("Hello EV World 3000")
 
-funny_word = os.environ['TestSecret']
-st.write(funny_word)
+#funny_word = os.environ['TestSecret']
+#st.write(funny_word)
+
